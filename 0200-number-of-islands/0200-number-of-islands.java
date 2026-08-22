6aea8f42345409b69f3c426ebc90f334752fef59
C A Sagar Varma <santoshpallavi107@gmail.com>
2026-08-22 10:42:06 +0530

    Time: 5 ms (28.55%), Space: 51 MB (97.58%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int count=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='1'){
+                   
+                    count++;
+                    bfs(grid,i,j);
+                }
+            }
+        }
+        return count;
+    }
+
+    public void bfs(char[][] grid,int row,int col){
+        Queue<int[]> q=new LinkedList<>();
+        q.add(new int[]{row,col});
+         grid[row][col]=0;
+        int [][]dircs={
+            {1,0},{0,1},{0,-1},{-1,0}
+        };
+
+        while(!q.isEmpty()){
+            int[] curr=q.remove();
+            int r=curr[0];
+            int c=curr[1];
+
+            for(int[]dir:dircs){
+                int nr=r+dir[0];
+                int nc=c+dir[1];
+                
+
+                if(nr>=0 && nr<grid.length&&nc>=0&&nc<grid[0].length&&grid[nr][nc]=='1'){
+                    grid[nr][nc]='0';
+                    q.add(new int[]{nr,nc});
+                }
+            }
+        }
+
+    }
+}

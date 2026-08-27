ef0782638584c5ab19f5fe76a2a1740220d8fdbe
C A Sagar Varma <santoshpallavi107@gmail.com>
2026-08-27 10:29:37 +0530

    Time: 3 ms (58.06%), Space: 45 MB (61.29%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.java
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.java
@@ -1,6 +1,6 @@
 class Solution {
     public String lexGreaterPermutation(String s, String target) {
-        
+
         int[] freq = new int[26];
 
         for (char c : s.toCharArray()) {
@@ -10,51 +10,72 @@ class Solution {
         StringBuilder ans = new StringBuilder();
 
         for (int i = 0; i < target.length(); i++) {
+
             int curr = target.charAt(i) - 'a';
 
-            // Try to match target
+            // Try to keep the same character
             if (freq[curr] > 0) {
                 freq[curr]--;
                 ans.append(target.charAt(i));
-            } else {
-                // Cannot match, try a greater character
-                int greater = findGreater(freq, curr);
+                continue;
+            }
+
+            // Same character is unavailable.
+            // Try the smallest character greater than target[i].
+            int greater = findGreater(freq, curr);
+
+            if (greater != -1) {
+                ans.append((char) ('a' + greater));
+                freq[greater]--;
+
+                addRemaining(ans, freq);
+
+                return ans.toString();
+            }
+
+            // Cannot continue -> backtrack
+            while (ans.length() > 0) {
+
+                int last = ans.length() - 1;
+                int previous = ans.charAt(last) - 'a';
+
+                // Put the character back
+                freq[previous]++;
+                ans.deleteCharAt(last);
+
+                // Try making this previous position bigger
+                greater = findGreater(freq, previous);
 
                 if (greater != -1) {
-                    ans.append((char)('a' + greater));
+                    ans.append((char) ('a' + greater));
                     freq[greater]--;
 
-                    appendSmallest(ans, freq);
+                    addRemaining(ans, freq);
+
                     return ans.toString();
                 }
-
-                // Backtrack
-                return backtrack(ans, freq);
             }
+
+            return "";
         }
 
-        // We matched the entire target exactly.
-        // Need to backtrack because answer must be STRICTLY greater.
-        return backtrack(ans, freq);
-    }
-
-    private String backtrack(StringBuilder ans, int[] freq) {
-
+        // We matched target exactly.
+        // Need a strictly greater permutation.
         while (ans.length() > 0) {
 
             int last = ans.length() - 1;
             int previous = ans.charAt(last) - 'a';
 
-            ans.deleteCharAt(last);
             freq[previous]++;
+            ans.deleteCharAt(last);
 
             int greater = findGreater(freq, previous);
 
             if (greater != -1) {
-                ans.append((char)('a' + greater));
+                ans.append((char) ('a' + greater));
                 freq[greater]--;
 
-                appendSmallest(ans, freq);
+                addRemaining(ans, freq);
 
                 return ans.toString();
             }
@@ -64,6 +85,7 @@ class Solution {
     }
 
     private int findGreater(int[] freq, int curr) {
+
         for (int i = curr + 1; i < 26; i++) {
             if (freq[i] > 0) {
                 return i;
@@ -73,10 +95,11 @@ class Solution {
         return -1;
     }
 
-    private void appendSmallest(StringBuilder ans, int[] freq) {
+    private void addRemaining(StringBuilder ans, int[] freq) {
+
         for (int i = 0; i < 26; i++) {
             while (freq[i] > 0) {
-                ans.append((char)('a' + i));
+                ans.append((char) ('a' + i));
                 freq[i]--;
             }
         }

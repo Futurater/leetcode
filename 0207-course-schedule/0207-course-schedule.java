42bc52ef09d67c63a452b58981ff6afc37f5dcff
C A Sagar Varma <santoshpallavi107@gmail.com>
2026-09-02 12:16:03 +0530

    Time: 719 ms (5.17%), Space: 287 MB (5.4%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj=new ArrayList<>();
+        int n=prerequisites.length;
+        for(int j=0;j<numCourses;j++){
+            adj.add(new ArrayList<>());
+        }
+
+        int []indegree=new int[numCourses];
+
+        for(int i=0;i<numCourses;i++){
+            for(int[]pair: prerequisites){
+                int course=pair[0];
+                int prereq=pair[1];
+
+                adj.get(prereq).add(course);
+                indegree[course]++;
+            }
+
+        }
+
+        
+
+
+        Queue<Integer> q= new LinkedList<>();
+        int course=0;
+        for(int i=0;i<indegree.length;i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+
+        while(!q.isEmpty()){
+            int c=q.remove();
+            course++;
+
+            for(int p:adj.get(c)){
+                indegree[p]--;
+                if(indegree[p]==0){
+                    q.add(p);
+                }
+            }
+        }
+        return course==numCourses;
+
+    }
+}
